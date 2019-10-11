0797f87faedf3e49627b6666e1e3919fb1e3d274
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-10-11 16:37:40 -0700

    Add type locale grouping separator

---
+++ src/types/locale_grouping_separator.rs
@@ -0,0 +1,57 @@
+//! Locale grouping separator, such as for delimiting numeric numbers.
+//!
+//! Examples:
+//!
+//!   * , is English
+//!   * TODO
+//!   * TODO
+
+use ::rand::{thread_rng, seq::SliceRandom};
+use crate::types::{text as supertype, text::Text as Supertype};
+
+pub type LocaleGroupingSeparator = Supertype;
+type T = LocaleGroupingSeparator;
+
+pub fn from_option_str(s: Option<&str>) -> Option<T> {
+    supertype::from_option_str(s)
+}
+
+lazy_static! {
+    static ref EXAMPLE_VEC: Vec<T> = {
+        vec![
+            T::from(","), // English
+            T::from("*"), // fake
+            T::from("~"), // fake
+        ]
+    };
+}
+
+pub fn fab() -> T {
+    EXAMPLE_VEC.choose(&mut thread_rng()).unwrap().clone()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::types::{locale_grouping_separator as t, locale_grouping_separator::LocaleGroupingSeparator as T};
+
+    #[test]
+    fn test_from_str() {
+        let s = "alpha";
+        let x: T = T::from(s);
+        assert_eq!(x, s);
+    }
+
+    #[test]
+    fn test_from_option_str() {
+        let s = "alpha";
+        let x: T = t::from_option_str(Some(s)).unwrap();
+        assert_eq!(x, s);
+    }
+
+    #[test]
+    fn test_fab() {
+        let x: T = t::fab();
+        assert!(!x.is_empty());
+    }
+
+}
