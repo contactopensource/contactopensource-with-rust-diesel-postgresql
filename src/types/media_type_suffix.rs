e06682ef9f4a6761d664c9a262f43dbdad9122a4
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-10-11 16:37:42 -0700

    Add type media type suffix

---
+++ src/types/media_type_suffix.rs
@@ -0,0 +1,55 @@
+//! Media type suffix
+//!
+//! Examples:
+//!
+//!   * +json
+//!   * +zip
+
+use ::rand::{thread_rng, seq::SliceRandom};
+use crate::types::{text as supertype, text::Text as Supertype};
+
+pub type MediaTypeSuffix = Supertype;
+type T = MediaTypeSuffix;
+
+pub fn from_option_str(s: Option<&str>) -> Option<T> {
+    supertype::from_option_str(s)
+}
+
+lazy_static! {
+    static ref EXAMPLE_VEC: Vec<T> = {
+        vec![
+            T::from("+json"),
+            T::from("+zip"),
+        ]
+    };
+}
+
+pub fn fab() -> T {
+    EXAMPLE_VEC.choose(&mut thread_rng()).unwrap().clone()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::types::{media_type_suffix as t, media_type_suffix::MediaTypeSuffix as T};
+
+    #[test]
+    fn test_from_str() {
+        let s = "alpha";
+        let x: T = T::from(s);
+        assert_eq!(x, s);
+    }
+
+    #[test]
+    fn test_from_option_str() {
+        let s = "alpha";
+        let x: T = t::from_option_str(Some(s)).unwrap();
+        assert_eq!(x, s);
+    }
+
+    #[test]
+    fn test_fab() {
+        let x: T = t::fab();
+        assert!(!x.is_empty());
+    }
+
+}
