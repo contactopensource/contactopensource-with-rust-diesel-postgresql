bf89b75f7a79551fdfa123efe5211a4015c71e1e
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-10-11 16:38:12 -0700

    Add media type fabricator

---
+++ src/models/media_type/fab.rs
@@ -0,0 +1,27 @@
+use crate::types;
+use crate::models::media_type::media_type::MediaType as T;
+
+pub fn fab() -> T {
+    T {
+        id: types::id::fab(),
+
+        // Programming-related
+        tenant_id: Some(types::id::fab()),
+        typecast: Some(types::typecast::fab()),
+        state: Some(types::state::fab()),
+
+        // Update-related
+        updated_at_timestamp_utc: Some(types::timestamp::fab()),
+        updated_at_clock_count: Some(types::count::fab()),
+        updated_by_text: Some(types::text::fab()),
+
+        // Content-related
+        text: Some(types::media_type_text::fab()),
+        supertype: Some(types::media_type_supertype::fab()),
+        subtype: Some(types::media_type_subtype::fab()),
+        tree: Some(types::media_type_tree::fab()),
+        suffix: Some(types::media_type_suffix::fab()),
+        parameters: Some(types::media_type_parameters::fab()),
+
+    }
+}
