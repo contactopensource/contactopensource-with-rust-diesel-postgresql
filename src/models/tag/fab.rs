a18bf1c6beb2749a7bfeab50cfb6601335a8f1bc
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-10-11 16:38:09 -0700

    Add tag fabricator

---
+++ src/models/tag/fab.rs
@@ -0,0 +1,22 @@
+use crate::types;
+use crate::models::tag::tag::Tag as T;
+
+pub fn fab() -> T {
+    T {
+        id: types::id::fab(),
+
+        // Programming-related
+        tenant_id: Some(types::id::fab()),
+        typecast: Some(types::typecast::fab()),
+        state: Some(types::state::fab()),
+
+        // Update-related
+        updated_at_timestamp_utc: Some(types::timestamp::fab()),
+        updated_at_clock_count: Some(types::count::fab()),
+        updated_by_text: Some(types::text::fab()),
+
+        // Link-related
+        text: Some(types::label::fab()),
+
+    }
+}
