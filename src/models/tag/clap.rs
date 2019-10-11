c2e32f609e1600befefb7070129a73fa53c07a97
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-10-11 16:38:09 -0700

    Add tag command line argument parser

---
+++ src/models/tag/clap.rs
@@ -0,0 +1,35 @@
+use clap::{App, Arg, ArgMatches};
+use crate::types;
+use crate::models::tag::tag::Tag as T;
+
+impl crate::traits::clap_able::ClapAble for T {
+
+    fn init_clap_app<'a, 'ar>(app: App<'a, 'ar>) -> App<'a, 'ar> {
+        app
+        .arg(Arg::with_name("text")
+            .help("text; example: \"trending\"")
+            .long("text")
+            .value_name("TEXT")
+            .takes_value(true))
+    }
+
+    fn from_clap_arg_matches(matches: &ArgMatches) -> T {
+        T {
+            id: types::id::from_option_str(matches.value_of("id")).unwrap(),
+
+            // Programming-related
+            tenant_id: types::id::from_option_str(matches.value_of("tenant_id")),
+            typecast: types::typecast::from_option_str(matches.value_of("typecast")),
+            state: types::state::from_option_str(matches.value_of("state")),
+
+            // Update-related
+            updated_at_timestamp_utc: types::timestamp::from_option_str(matches.value_of("updated_at_timestamp_utc")),
+            updated_at_clock_count: types::count::from_option_str(matches.value_of("updated_at_clock_count")),
+            updated_by_text: types::text::from_option_str(matches.value_of("updated_by_text")),
+
+            // Link-related
+            text: types::text::from_option_str(matches.value_of("text")),
+        }
+    }
+
+}
