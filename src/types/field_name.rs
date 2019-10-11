0363aaa338b0ef24cdeb83453ceef65529ca28d7
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-10-11 16:37:38 -0700

    Add type field name

---
+++ src/types/field_name.rs
@@ -0,0 +1,42 @@
+//! Typical database field name.
+//!
+//! Example: "updated_at" is a typical database field `updated_at`.
+
+use crate::types::{self, text as supertype, text::Text as Supertype};
+
+pub type FieldName = Supertype;
+type T = FieldName;
+
+pub fn from_option_str(s: Option<&str>) -> Option<T> {
+    supertype::from_option_str(s)
+}
+
+pub fn fab() -> T {
+    types::alpha_word::fab()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::types::{field_name as t, field_name::FieldName as T};
+
+    #[test]
+    fn test_from_str() {
+        let s = "alpha";
+        let x: T = T::from(s);
+        assert_eq!(x, s);
+    }
+
+    #[test]
+    fn test_from_option_str() {
+        let s = "alpha";
+        let x: T = t::from_option_str(Some(s)).unwrap();
+        assert_eq!(x, s);
+    }
+
+    #[test]
+    fn test_fab() {
+        let x: T = t::fab();
+        assert!(!x.is_empty());
+    }
+
+}
