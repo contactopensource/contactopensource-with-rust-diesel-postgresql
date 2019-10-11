e2b015807a03c30aa228cac2e8faabefdfaca5d2
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-10-11 16:37:49 -0700

    Add type street address text

---
+++ src/types/street_address_text.rs
@@ -0,0 +1,44 @@
+//! Street address text.
+//!
+//! Example: "100 Main Street"
+//!
+//! Note: this is freeform text, and also suitable for user input.
+
+use crate::types::{self, text as supertype, text::Text as Supertype};
+
+pub type StreetAddressText = Supertype;
+type T = StreetAddressText;
+
+pub fn from_option_str(s: Option<&str>) -> Option<T> {
+    supertype::from_option_str(s)
+}
+
+pub fn fab() -> T {
+    types::alpha_word::fab()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::types::{street_address_text as t, street_address_text::StreetAddressText as T};
+
+    #[test]
+    fn test_from_str() {
+        let s = "a";
+        let x: T = T::from(s);
+        assert_eq!(x, s);
+    }
+
+    #[test]
+    fn test_from_option_str() {
+        let s = "a";
+        let x: T = t::from_option_str(Some(s)).unwrap();
+        assert_eq!(x, s);
+    }
+
+    #[test]
+    fn test_fab() {
+        let x: T = t::fab();
+        assert!(!x.is_empty());
+    }
+
+}
