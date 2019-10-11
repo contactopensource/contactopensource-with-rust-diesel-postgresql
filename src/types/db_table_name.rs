f7b5d97387c36d01aca435359d84c8c49ea69fc5
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-10-11 16:37:36 -0700

    Add type db table name

---
+++ src/types/db_table_name.rs
@@ -0,0 +1,42 @@
+//! DB table name.
+//!
+//! Example: "users" means DB table `users`.
+
+use crate::types::{self, text as supertype, text::Text as Supertype};
+
+pub type DbTableName = Supertype;
+type T = DbTableName;
+
+pub fn from_option_str(s: Option<&str>) -> Option<T> {
+    supertype::from_option_str(s)
+}
+
+pub fn fab() -> T {
+    types::alpha_word::fab()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::types::{db_table_name as t, db_table_name::DbTableName as T};
+
+    #[test]
+    fn test_from_str() {
+        let s = "alpha";
+        let x: T = T::from(s);
+        assert_eq!(x, s);
+    }
+
+    #[test]
+    fn test_from_option_str() {
+        let s = "alpha";
+        let x: T = t::from_option_str(Some(s)).unwrap();
+        assert_eq!(x, s);
+    }
+
+    #[test]
+    fn test_fab() {
+        let x: T = t::fab();
+        assert!(!x.is_empty());
+    }
+
+}
