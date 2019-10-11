986a498ed5584ac90fb6b2dd9606b572a023b7ce
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-10-11 16:38:01 -0700

    Add link contact fabricator

---
+++ src/models/link_contact/fab.rs
@@ -0,0 +1,23 @@
+use crate::types;
+use crate::models::link_contact::link_contact::LinkContact as T;
+
+pub fn fab() -> T {
+    T {
+        id: types::id::fab(),
+
+        // Programming-related
+        tenant_id: Some(types::id::fab()),
+        typecast: Some(types::typecast::fab()),
+        state: Some(types::state::fab()),
+
+        // Update-related
+        updated_at_timestamp_utc: Some(types::timestamp::fab()),
+        updated_at_clock_count: Some(types::count::fab()),
+        updated_by_text: Some(types::text::fab()),
+
+        // Link-related
+        label: Some(types::label::fab()),
+        uri: Some(types::uri_string::fab()),
+
+    }
+}
