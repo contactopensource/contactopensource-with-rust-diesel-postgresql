ab889664b30780378a6827ad2ae379eb9abebb15
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-10-11 16:37:49 -0700

    Add type subject pronoun

---
+++ src/types/subject_pronoun.rs
@@ -0,0 +1,58 @@
+//! Subject pronoun.
+//!
+//! See: TODO
+//!
+//! Example: TODO
+
+use ::rand::{thread_rng, seq::SliceRandom};
+use crate::types::{text as supertype, text::Text as Supertype};
+
+pub type SubjectPronoun = Supertype;
+type T = SubjectPronoun;
+
+pub fn from_option_str(s: Option<&str>) -> Option<T> {
+    supertype::from_option_str(s)
+}
+
+lazy_static! {
+    static ref EXAMPLE_VEC: Vec<T> = {
+        vec![
+            T::from("he"),
+            T::from("she"),
+            T::from("they"),
+            T::from("e"),
+            T::from("ve"),
+            T::from("xe"),
+        ]
+    };
+}
+
+pub fn fab() -> T {
+    EXAMPLE_VEC.choose(&mut thread_rng()).unwrap().clone()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::types::{subject_pronoun as t, subject_pronoun::SubjectPronoun as T};
+
+    #[test]
+    fn test_from_str() {
+        let s = "a";
+        let x: T = T::from(s);
+        assert_eq!(x, s);
+    }
+
+    #[test]
+    fn test_from_option_str() {
+        let s = "a";
+        let x: T = t::from_option_str(Some(s)).unwrap();
+        assert_eq!(x, s);
+    }
+
+    #[test]
+    fn test_fab() {
+        let x: T = t::fab();
+        assert!(!x.is_empty());
+    }
+
+}
