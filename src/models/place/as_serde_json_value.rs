605534d2a3e2ef1be7ee0e7011d9d017fdac74ff
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-10-11 16:37:58 -0700

    Add place trait AsSerdeJsonValue

---
+++ src/models/place/as_serde_json_value.rs
@@ -0,0 +1,25 @@
+use serde_json::json;
+use crate::models::place::place::Place;
+
+impl crate::traits::as_serde_json_value::AsSerdeJsonValue for Place {
+
+    fn as_serde_json_value(&self) -> serde_json::Value {
+        json!({
+            "id": self.id,
+
+            // Programming-related
+            "tenant_id": self.tenant_id.clone().unwrap().to_owned(),
+            "typecast": self.typecast.clone().unwrap().to_owned(),
+            "state": self.state.clone().unwrap().to_owned(),
+
+            // Update-related
+            "updated_at_timestamp_utc": self.updated_at_timestamp_utc.clone().unwrap().to_owned(),
+            "updated_at_clock_count": self.updated_at_clock_count.clone().unwrap().to_owned(),
+            "updated_by_text": self.updated_by_text.clone().unwrap().to_owned(),
+
+            // Name-related
+            "name": self.name.clone().unwrap().to_owned(),
+        })
+    }
+
+}
