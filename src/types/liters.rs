3958ae2461c00f779391fca81650f1cba77f4690
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-10-11 16:37:40 -0700

    Add type liters

---
+++ src/types/liters.rs
@@ -0,0 +1,46 @@
+//! Measurement metrics that use liters, such as for volume.
+//!
+//! Example: 8 means 8 liters.
+
+use crate::types::{metrics as supertype, metrics::Metrics as Supertype};
+
+pub type Liters = Supertype;
+type T = Liters;
+
+pub fn from_option_str(s: Option<&str>) -> Option<T> {
+    supertype::from_option_str(s)
+}
+
+pub fn fab() -> T {
+    supertype::fab()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::types::{liters as t, liters::Liters as T};
+    use ::bigdecimal::Zero;
+    use ::std::str::FromStr;
+
+    #[test]
+    fn test_from_str() {
+        let s = "0";
+        let e = T::zero();
+        let x: T = T::from_str(&s).unwrap();
+        assert_eq!(x, e);
+    }
+
+    #[test]
+    fn test_from_option_str() {
+        let s = "0";
+        let e = T::zero();
+        let x: T = t::from_option_str(Some(s)).unwrap();
+        assert_eq!(x, e);
+    }
+
+    #[test]
+    fn test_fab() {
+        let _x: T = t::fab();
+    }
+
+}
