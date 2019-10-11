8965c37ac34d8f09b6099f4862970221f528a4e9
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-10-11 16:38:15 -0700

    Add geo code fabricator

---
+++ src/models/geo_code/fab.rs
@@ -0,0 +1,26 @@
+use crate::types;
+use crate::models::geo_code::geo_code::GeoCode as T;
+
+pub fn fab() -> T {
+    T {
+        id: types::id::fab(),
+
+        // Programming-related
+        tenant_id: Some(types::id::fab()),
+        typecast: Some(types::typecast::fab()),
+        state: Some(types::state::fab()),
+
+        // Update-related
+        updated_at_timestamp_utc: Some(types::timestamp::fab()),
+        updated_at_clock_count: Some(types::count::fab()),
+        updated_by_text: Some(types::text::fab()),
+
+        // Code-related
+        coder_id: Some(types::id::fab()),
+        text: Some(types::text::fab()),
+        latitude: Some(types::latitude::fab()),
+        longitude: Some(types::longitude::fab()),
+        altitude: Some(types::altitude::fab()),
+        elevation: Some(types::elevation::fab()),
+    }
+}
