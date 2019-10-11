02ab12adc71a0d061f007c2c84e5aa7fb1971264
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-10-11 16:37:55 -0700

    Add org trait AsSerdeJsonValue

---
+++ src/models/org/as_serde_json_value.rs
@@ -0,0 +1,26 @@
+use serde_json::json;
+use crate::models::org::org::Org;
+
+impl crate::traits::as_serde_json_value::AsSerdeJsonValue for Org {
+
+    fn as_serde_json_value(&self) -> serde_json::Value {
+        json!({
+            "id": self.id,
+
+            // Programming-related
+            "tenant_id": self.tenant_id.clone().unwrap().to_owned(),
+            "typecast": self.typecast.clone().unwrap().to_owned(),
+            "state": self.state.clone().unwrap().to_owned(),
+
+            // Update-related
+            "updated_at_timestamp_utc": self.updated_at_timestamp_utc.clone().unwrap().to_owned(),
+            "updated_at_clock_count": self.updated_at_clock_count.clone().unwrap().to_owned(),
+            "updated_by_text": self.updated_by_text.clone().unwrap().to_owned(),
+
+            // Lifetime-related
+            "start_date": self.start_date.clone().unwrap().to_owned(),
+            "stop_date": self.stop_date.clone().unwrap().to_owned(),
+        })
+    }
+
+}
